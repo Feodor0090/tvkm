dd2eaaac3c42869627b6ece38a741f3aeed1ed4d
ansel <79257300125@ya.ru>
2022-05-26 01:00:35 +0300

    Implement settings editing

--- tvkm/ConfigView.cs
+++ tvkm/ConfigView.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using tvkm.UIEngine;
 using tvkm.UIEngine.Controls;
 using tvkm.UIEngine.Templates;
@@ -14,15 +15,46 @@ public class ConfigView : ListScreen<App>
     {
         ConfigManager.ReadConfig();
         base.OnEnter(stack);
-        
         Clear();
         var fields = typeof(Settings).GetFields();
         foreach (var field in fields)
         {
-            Add(new Label<App>($"{field.Name}: {field.GetValue(null)?.ToString() ?? "<NULL>"}"));
+            Add(new EditableConfigLine(field));
         }
 
-        Add(new Label<App>(""));
-        Add(new Label<App>("Пока только для чтения. Редактируйте config.txt"));
+        Add(new Button<App>("Сохранить всё в файл конфигурации", ConfigManager.SaveConfig));
+        Add(new Button<App>("Сброс", () =>
+        {
+            ConfigManager.CreatePlaceholderConfig();
+            while (!stack.Empty)
+                stack.Back();
+        }));
+    }
+
+    protected class EditableConfigLine : Label<App>
+    {
+        private readonly FieldInfo _field;
+
+        public EditableConfigLine(FieldInfo field) : base($"{field.Name}: {field.GetValue(null)?.ToString() ?? "null"}")
+        {
+            _field = field;
+        }
+
+        public override void HandleKey(InputEvent e, ScreenStack<App> stack)
+        {
+            if (e.Action == InputAction.Activate)
+            {
+                stack.Push(new TextboxPopup<App>("Редактирование настройки", _field.Name,
+                    _field.GetValue(null)?.ToString() ?? "null",
+                    s =>
+                    {
+                        var dict = new Dictionary<string, string>();
+                        dict.Add(_field.Name, s);
+                        Settings.Apply(dict);
+                        Text = $"{_field.Name}: {_field.GetValue(null)?.ToString() ?? "null"}";
+                        stack.Back();
+                    }));
+            }
+        }
     }
 }
